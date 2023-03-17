5a2ad661d3446fa929bc64d40689ae48f0a59ab0
egorb <egorbaturin2013@yandex.ru>
2023-03-17 17:03:18 +0300

    Initial commit

---
+++ src/main/java/com/crm/Tiefling/TieflingApplication.java
@@ -0,0 +1,13 @@
+package com.crm.Tiefling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TieflingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TieflingApplication.class, args);
+	}
+
+}
